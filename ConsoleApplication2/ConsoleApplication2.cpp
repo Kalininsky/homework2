e8ce03e6ec77120d16b93fbcfde86b924afa8eff
Kirill <Kalininsky14@mail.ru>
2021-09-14 18:12:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	//task 1
+
+	setlocale(LC_ALL, "Rus");
+	int a, b;
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << (((a + b) >= 10 && (a + b) <= 20) ? "true" : "false") << std::endl;
+
+
+{
+		//task 2
+
+		const int c = 10, d = 7;
+		std::cout << (((a == 10 && b == 10) || (a + b) == 10) ? "true" : "false") << std::endl;
+	}
+
+{
+	//task 3
+	std::cout << "Your numbers: ";
+	for (int i = 1; i <= 50; i += 2) {
+      std::cout << i;
+	}
+	std::cout << std::endl;
+
+
+}
+
+return 0;
+
+}
